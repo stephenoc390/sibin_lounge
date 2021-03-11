82c808c707e1391ea1a12579b8f3f23362416f7b
stephenoc390 <stephenoconnor390@gmail.com>
2021-03-11 02:25:08 +0000

    created add_drink template

--- app.py
+++ app.py
@@ -99,6 +99,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_drink")
+def add_drink():
+    return render_template("add_drink.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
